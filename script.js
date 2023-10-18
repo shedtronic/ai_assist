b7656ecb93fd451702df08351e7aab2c6d4e2a17
Shedtronic <38550478+shedtronic@users.noreply.github.com>
2023-10-18 21:55:08 +0100

    Updated Javascript and css

--- script.js
+++ script.js
@@ -1,69 +1,62 @@
-
 let recognition;
 let recognizedWords = [];
 
 function setup() {
-let outputDiv = document.getElementById('output');
-outputDiv.style.display = 'none';
+  const outputDiv = document.getElementById('output');
+  outputDiv.style.position = 'relative';
 
-const startButton = document.getElementById('startButton');
-const stopButton = document.getElementById('stopButton');
+  const startButton = document.getElementById('startButton');
+  const stopButton = document.getElementById('stopButton');
 
-startButton.addEventListener('click', startListening);
-stopButton.addEventListener('click', stopListening);
+  startButton.addEventListener('click', startListening);
+  stopButton.addEventListener('click', stopListening);
 
-if ('webkitSpeechRecognition' in window || 'SpeechRecognition' in window) {
-  recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
-  recognition.continuous = true;
-  recognition.interimResults = true;
+  if ('webkitSpeechRecognition' in window || 'SpeechRecognition' in window) {
+    recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
+    recognition.continuous = true;
+    recognition.interimResults = true;
 
-  recognition.onresult = function (event) {
-    recognizedWords = [];
-    for (let i = 0; i < event.results.length; i++) {
-      recognizedWords.push(event.results[i][0].transcript);
-    }
-    displayWords();
-  };
-} else {
-  console.log('Speech recognition is not supported in this browser.');
+    recognition.onresult = function (event) {
+      for (let i = 0; i < event.results.length; i++) {
+        const word = event.results[i][0].transcript;
+        recognizedWords.push(word);
+        displayWord(word);
+      }
+    };
+  } else {
+    console.log('Speech recognition is not supported in this browser.');
+  }
 }
-}
 
-function displayWords() {
-let outputDiv = document.getElementById('output');
-outputDiv.style.display = 'block';
-outputDiv.textContent = '';
-
-// Randomly position each word within the canvas
-let canvasWidth = window.innerWidth - 40; // Account for 20px padding
-let canvasHeight = window.innerHeight - 40; // Account for 20px padding
-
-recognizedWords.forEach((word) => {
-  let wordElement = document.createElement('span');
+function displayWord(word) {
+  const outputDiv = document.getElementById('output');
+  const wordElement = document.createElement('span');
   wordElement.textContent = word;
+
+  // Randomly position the word within the canvas
+  const canvasWidth = outputDiv.offsetWidth;
+  const canvasHeight = outputDiv.offsetHeight;
   wordElement.style.position = 'absolute';
   wordElement.style.left = `${Math.floor(Math.random() * canvasWidth)}px`;
   wordElement.style.top = `${Math.floor(Math.random() * canvasHeight)}px`;
   wordElement.classList.add('text-fade');
   outputDiv.appendChild(wordElement);
-});
 
-// Clear words after 5 seconds
-setTimeout(() => {
-  recognizedWords = [];
-  outputDiv.style.display = 'none';
-  outputDiv.textContent = '';
-}, 5000);
+  // Clear the word after 5 seconds
+  setTimeout(() => {
+    recognizedWords.shift();
+    outputDiv.removeChild(wordElement);
+  }, 5000);
 }
 
 function startListening() {
-if (recognition) {
-  recognition.start();
-}
+  if (recognition) {
+    recognition.start();
+  }
 }
 
 function stopListening() {
-if (recognition) {
-  recognition.stop();
-}
+  if (recognition) {
+    recognition.stop();
+  }
 }
